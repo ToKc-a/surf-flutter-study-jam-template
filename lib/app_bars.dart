9a50b62a1070e709110a991e9759008af66882fe
ToKc <ant.tokc@gmail.com>
2022-02-20 12:48:34 +0200

    add appbar & bottomAppBar

---
+++ lib/app_bars.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+PreferredSizeWidget chatAppBar() {
+  return AppBar(
+    title: TextFormField(
+      style: TextStyle(color: Colors.white),
+      autofocus: true,
+      decoration: InputDecoration(
+        labelText: 'Введите ник',
+        labelStyle: TextStyle(color: Colors.white),
+        suffixIcon: GestureDetector(
+          onTap: () {},
+          child: Icon(
+            Icons.refresh,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+Widget chatBottomAppBar() {
+  return BottomAppBar(
+    child: TextFormField(
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.all(16.0),
+        labelText: 'Сообщение',
+        suffixIcon: GestureDetector(
+          onTap: () {},
+          child: Icon(
+            Icons.send,
+          ),
+        ),
+      ),
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,8 @@ void main() async {
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform(
-      androidKey: 'enter android key here',
-      iosKey: 'enter ios key here',
+      androidKey: 'AIzaSyCXU5f25S_SUeVv7cAzoeF373kHk4Hv2dE',
+      iosKey: 'AIzaSyAjvNoe31ZMSakjKFOkhbrZBsj3RyW6plo',
       webKey: 'enter web key here',
     ),
   );
@@ -27,6 +27,7 @@ class MyApp extends StatelessWidget {
     final chatRepository = ChatRepositoryFirebase(FirebaseFirestore.instance);
 
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorSchemeSeed: Colors.deepPurple,
         useMaterial3: true,
